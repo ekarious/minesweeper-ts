b59ff1b6030b1e57012b28fdad90e0699ab04d02
Kitefr <kitefr@gmail.com>
2018-05-23 15:41:28 +0200

    Base game added without logic

---
+++ src/js/main.js
@@ -0,0 +1,31 @@
+const options = {};
+
+/**
+ * Game class
+ *
+ * @class Game
+ */
+class Game {
+  constructor() {}
+  start() {}
+  stop() {}
+}
+
+/**
+ * Board class
+ *
+ * @class Board
+ */
+class Board {}
+
+/**
+ * Tile class
+ *
+ * @class Tile
+ */
+class Tile {}
+
+// Events
+window.addEventListener("load", () => {});
+
+Game.init();
